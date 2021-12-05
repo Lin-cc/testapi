3a51fe08b9a468901e46955452cacab85478b229
Lin <lh6688@qq.com>
2021-12-05 22:32:06 +0800

    setswaggeralways

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "dotnetdemo v1"));
